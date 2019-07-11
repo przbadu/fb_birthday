bfcef07cf014b0d6dbed7c632acde8d434a3a08d
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2019-07-11 10:49:45 +0545

    add scope for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], callback_url: "https://fbirthday.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook,
+    ENV['FB_APP_ID'],
+    ENV['FB_APP_SECRET'],
+    scope: 'email,user_birthday',
+    callback_url: "https://fbirthday.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
